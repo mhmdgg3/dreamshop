1d02c394cd0f9a9e53ac39563ff4980363a9606c
mhmdgg3 <68659385+mhmdgg3@users.noreply.github.com>
2025-04-17 19:49:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,28 @@
-let catrgory_nav_list = document.querySelector(".catrgory_nav_list");
-
-function open_cate_list(){
-    catrgory_nav_list.classList.toggle("active")
-}
-
-
-
-var cart = document.querySelector('.cart');
-
-function open_close_cart() {
-
-    cart.classList.toggle("active")
-    
-}
-
 
 fetch('products.json')
-.then(Response => Response.json())
-.then (data => {
-    const addToCartButtons = document.querySelectorAll(".btn_add_card")
+.then(response => response.json())
+.then(data => {
+    
+    const addToCartButtons = document.querySelectorAll(".btn_add_cart")
 
     addToCartButtons.forEach(button =>{
-
         button.addEventListener("click", (event) => {
             const productId = event.target.getAttribute('data-id')
             const selcetedProduct = data.find(product => product.id == productId)
-
-
+            
 
             addToCart(selcetedProduct)
 
-            const allMatchingButtons = document.querySelectorAll(`.btn_add_card[data-id="${productId}"]`)
-            allMatchingButtons.forEach(btn => {
+            const allMatchingButtons = document.querySelectorAll(`.btn_add_cart[data-id="${productId}"]`)
+
+            allMatchingButtons.forEach(btn =>{
                 btn.classList.add("active")
-                btn.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> Item in card </span>
-                `
+                btn.innerHTML = `      <i class="fa-solid fa-cart-shopping"></i> Item in cart`
             })
         })
-
-
-    })    
-
-
+    })
+    
+    
 })
 
 
@@ -52,41 +33,47 @@ function addToCart(product) {
     cart.push({... product , quantity: 1})
     localStorage.setItem('cart' , JSON.stringify(cart))
 
+
     updateCart()
-    
 }
 
-function updateCart() {
 
+
+function updateCart() {
     const cartItemsContainer = document.getElementById("cart_items")
 
     const cart = JSON.parse(localStorage.getItem('cart')) || []
 
-    cartItemsContainer.innerHTML = "" ;
 
+    var total_Price = 0
+    var total_count = 0
+
+    cartItemsContainer.innerHTML = "" ;
     cart.forEach((item , index) => {
 
-        cartItemsContainer.innerHTML += `
+        let total_Price_item = item.price * item.quantity;
 
-        <div class="item_card">
+        total_Price += total_Price_item
+        total_count += item.quantity
+
+    
+        cartItemsContainer.innerHTML += `
+        
+            <div class="item_cart">
                 <img src="${item.img}" alt="">
                 <div class="content">
                     <h4>${item.name}</h4>
-                    <p class="price_cart">$${item.price}</p>
-                    <div class="quntity_control">
-                        <button class="decrease_quantity">-</button>
+                    <p class="price_cart">$${total_Price_item}</p>
+                    <div class="quantity_control">
+                        <button class="decrease_quantity" data-index=${index}>-</button>
                         <span class="quantity">${item.quantity}</span>
-                        <button class="increase_quantity">+</button>
-
+                        <button class="Increase_quantity" data-index=${index}>+</button>
                     </div>
                 </div>
 
-                <button class="delete_item"><i class="fa-solid fa-trash-can"></i></button>
+                <button class="delete_item" data-inex="${index}" ><i class="fa-solid fa-trash-can"></i></button>
             </div>
-        
-        
+
+
         `
-
     })
-}
-
