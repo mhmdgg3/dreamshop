066d1c700cb3312afd783420d5410672105894ff
mhmdgg3 <68659385+mhmdgg3@users.noreply.github.com>
2025-04-17 19:20:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ function addToCart(product) {
 
     let cart = JSON.parse(localStorage.getItem('cart')) || []
 
-    cart.push({ ...product , quantity: 1})
+    cart.push({... product , quantity: 1})
     localStorage.setItem('cart' , JSON.stringify(cart))
 
     updateCart()
